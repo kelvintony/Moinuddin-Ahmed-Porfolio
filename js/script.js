7684c8ba17680a29ff5293ab3ace59ac8284f26b
Jony <gamingbir@gmail.com>
2020-07-16 23:25:18 -0400

    fixed the arrows and images pop up

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ for (let i = 0; i < totalFilterbtn; i++) {
   filterBtns[i].addEventListener("click", function () {
     filterContainer.querySelector(".active").classList.remove("active");
     this.classList.add("active");
-
+    //trying to show or hide according to what picture is selected
     const filterValue = this.getAttribute("data-filter");
     for (let k = 0; k < totalPortfolioItem; k++) {
       if (filterValue === portfolioItems[k].getAttribute("data-category")) {
@@ -27,3 +27,55 @@ for (let i = 0; i < totalFilterbtn; i++) {
     }
   });
 }
+
+//Portfolio Lightbox
+
+const lightbox = document.querySelector(".lightbox"),
+  lightboxImg = lightbox.querySelector(".lightbox-img"),
+  lightboxClose = lightbox.querySelector(".lightbox-close"),
+  lightboxText = lightbox.querySelector(".caption-text"),
+  lightboxCounter = lightbox.querySelector(".caption-counter");
+let itemIndex = 0;
+
+for (let i = 0; i < totalPortfolioItem; i++) {
+  portfolioItems[i].addEventListener("click", function () {
+    itemIndex = i;
+    changeItem();
+    toggleLightBox();
+  })
+}
+//go next item
+function nextItem() {
+  if (itemIndex === totalPortfolioItem - 1) {
+    itemIndex = 0;
+  } else {
+    itemIndex++;
+  }
+  changeItem();
+}
+//go prevItem
+function prevItem() {
+  if (itemIndex == 0) {
+    itemIndex = totalPortfolioItem - 1;
+  } else {
+    itemIndex--;
+  }
+  changeItem();
+}
+//toggle close/open the image 
+function toggleLightBox() {
+  lightbox.classList.toggle("open");
+}
+
+function changeItem() {
+  imgSrc = portfolioItems[itemIndex].querySelector(".portfolio-image img").getAttribute("src");
+  lightboxImg.src = imgSrc;
+  lightboxText.innerHTML = portfolioItems[itemIndex].querySelector("h4").innerHTML;
+  lightboxCounter.innerHTML = (itemIndex + 1) + " of " + totalPortfolioItem;
+}
+
+//close lightbox
+
+lightbox.addEventListener("click", function (event) {
+
+})
