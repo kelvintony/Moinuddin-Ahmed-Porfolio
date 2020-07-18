8e97d825527c15dd226bfd87d25cecb14ad93242
Jony <gamingbir@gmail.com>
2020-07-17 21:37:40 -0400

    adding styleSwticher

---
+++ js/styleSwitcher.js
@@ -0,0 +1,16 @@
+
+
+const links = document.querySelectorAll(".alternate-style"),
+    totalLinks = links.length;
+
+function setActiveStyle(color) {
+    for (let i = 0; i < totalLinks; i++) {
+        if (color == links[i].getAttribute("title")) {
+            links[i].removeAttribute("disabled");
+            
+        } else {
+            links[i].setAttribute("disabled");
+            console.log("disabled")
+        }
+    }
+}
