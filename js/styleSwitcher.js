952f8d5c91d54a497634bcde4c2e57de2102f1dc
Jony <gamingbir@gmail.com>
2020-07-18 00:23:05 -0400

    fnished section 8
    everything working as intended and now linking the buttons.

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -1,18 +1,35 @@
-
-
+//skin color selection
 const links = document.querySelectorAll(".alternate-style"),
     totalLinks = links.length;
 
 function setActiveStyle(color) {
     for (let i = 0; i < totalLinks; i++) {
-        if (color == links[i].getAttribute("title")) {
+        if (color === links[i].getAttribute("title")) {
             //links[i].removeAttribute("disabled");
             links[i].disabled = false;
-
         } else {
             //links[i].setAttribute("disabled");
             links[i].disabled = true;
-            console.log("disabled")
+            console.log("disabled");
         }
     }
 }
+
+//Body Skin
+const bodySkin = document.querySelectorAll(" .body-skin"),
+    totalBodySkin = bodySkin.length;
+for (let i = 0; i < totalBodySkin; i++) {
+    bodySkin[i].addEventListener("change", function () {
+        if (this.value === "dark") {
+            document.body.className = "dark";
+        } else {
+            document.body.className = "light";
+        }
+    });
+}
+//coggle
+document
+    .querySelector(".toggle-style-swticher")
+    .addEventListener("click", () => {
+        document.querySelector(".style-switcher").classList.toggle("open");
+    });
